6bc9f78d12198486cc9ccb840471c97ca8d9ca35
fbehrendt <behrendt.fabian1@web.de>
2021-05-21 16:22:16 +0200

    Users that have typed a message in the past x minutes are considered active and added a concept of saturation that restores health, but decreases the ability to attack/evade

--- src/the-ban-game.py
+++ src/the-ban-game.py
@@ -32,7 +32,10 @@ IMG_LEADER = 'HypeFighter'
 
 # Game mechanic parameters
 MAX_HP = 50
-BAN_COOLDOWN_SEC = 10
+BAN_COOLDOWN_SEC = 60
+ACTIVE_CHATTER_TIMEOUT = 300
+SATURATION_COOLDOWN = 2
+
 
 # Player main object
 class Player:
@@ -44,6 +47,8 @@ class Player:
         self.kills = 0
         self.last_ban_ts = 0
         self.weakness = 0
+        self._saturation = 0  # slowly recharges health, but adds slowness. Slowness is subtracted from the dice roll
+        self.last_saturation = time()
 
     def life(self):
         return self._life
@@ -54,38 +59,74 @@ class Player:
     def damage(self, life):
         self._life = max(self._life - life, 0)
 
+    def saturate(self, saturation):
+        self._saturation = min(self._saturation + saturation, 50)
+        self.last_saturation = time()
+
+    def restoreHealth(self):
+        delta_t = time() - self.last_saturation
+        restored = min(self._saturation, int(delta_t / SATURATION_COOLDOWN))
+        self._life = min(self._life + restored, MAX_HP)
+        self._saturation = max(self._saturation - restored, 0)
+        self.last_saturation = time()
+
+    def saturation(self):
+        return self._saturation
+
+
 # Store all players statistics here
 players = {
 }
+
+# {user: last_msg_timestamp}
+# users added on message received
+# remove users in is_valid_user() method before check
+active_chatters = {}
+
 
 @bot.event
 async def event_ready():
     print(f"{os.environ['BOT_NICK']} is online!")
 
+
 @bot.event
 async def event_message(msg):
-    life = msg.content.count(IMG_REVIVE)
-    if life > 0:
-        # Heal player if the message contains the IMG_REVIVE emote
+    # add the user as active/update the timestamp
+    active_chatters[msg.author.name] = int(time())
+    saturation = msg.content.count(IMG_REVIVE)
+    if saturation > 0:  # msg.author.is_subscriber and
+        # Add one saturation for every IMG_REVIVE emote in the message
+        # Don't punish users too much for using IMG_REVIVE => 5 max
         player = msg.author.name
         init_player(player)
-        if msg.author.is_subscriber:
-            players[player].heal(life)
+        players[player].saturate(max(saturation, 5))
     await bot.handle_commands(msg)
+
 
 def init_player(player):
     if player not in players:
         players[player] = Player()
 
+
 def parse_args(ctx, command):
     return ctx.message.content.lower().removeprefix(command).strip().strip('@').split(' ')[0]
 
-def is_valid_user(name):
+
+async def is_valid_user(name):
+    # remove users from active chatters list if they were inactive for too long
+    for chatter in active_chatters:
+        delta_t = int(time() - active_chatters[chatter])
+        if delta_t > ACTIVE_CHATTER_TIMEOUT:
+            del active_chatters[chatter]
     if name == '':
         return False
-    r = get(f'http://tmi.twitch.tv/group/user/{STREAMER_NAME}/chatters')
-    users = json.loads(r.text)['chatters']
-    return name in itertools.chain.from_iterable([users[i] for i in users])
+    users = list(await bot.get_chatters(STREAMER_NAME))[1]
+    result = (name in users) or (name in list(active_chatters.keys()))
+    if result:
+        init_player(name)
+        players[name].restoreHealth()
+    return result
+
 
 def check_weakness(player, do_ban=False):
     delta_t = time() - players[player].last_ban_ts
@@ -94,6 +135,7 @@ def check_weakness(player, do_ban=False):
     elif do_ban:
         players[player].weakness += 1
     return players[player].weakness
+
 
 @bot.command(name='score')
 async def score(ctx):
@@ -105,33 +147,34 @@ async def score(ctx):
         first_place = ' |'
     await ctx.send(score)
 
+
 @bot.command(name='life')
 async def life(ctx):
     player = ctx.author.name
     target = parse_args(ctx, '!life')
 
-    if target != '' and not is_valid_user(target):
+    if target != '' and not await is_valid_user(target):
         await ctx.send(f"@{player} is trying to inspect someone that has long left the area {IMG_NONE}")
         return
 
     init_player(player)
-    if is_valid_user(target):
-        init_player(target)
-    else:
+    if not await is_valid_user(target):
         target = player
+    players[target].restoreHealth()
     hp = players[target].life()
     if hp == 0:
         await ctx.send(f"@{target} is banned {IMG_RIP}")
     else:
         weakness = check_weakness(target)
-        await ctx.send(f"@{target} has {hp} {IMG_HEALTH} left, weakness is {weakness}")
+        await ctx.send(f"@{target} has {hp} {IMG_HEALTH} left, weakness is {weakness}, saturation is {players[target].saturation()}")
+
 
 @bot.command(name='unban')
 async def unban(ctx):
     player = ctx.author.name
     target = parse_args(ctx, '!unban')
 
-    if not is_valid_user(target):
+    if not await is_valid_user(target):
         await ctx.send(f"@{player} casts unban at someone that has long left the area {IMG_NONE}")
         return
 
@@ -150,17 +193,19 @@ async def unban(ctx):
         await ctx.send(f"@{player} tried to cast healing from the realm of the banned {IMG_RIP}")
         return
 
-    await ctx.send(f"@{player} spectacularly fails at casting an ancient healing spell that they haven't practiced enough {IMG_FAIL}")
+    await ctx.send(
+        f"@{player} spectacularly fails at casting an ancient healing spell that they haven't practiced enough {IMG_FAIL}")
     players[player].damage(1)
     if players[player].life() == 0:
         await ctx.send(f"@{player} died trying to cast healing at {target} {IMG_RIP}")
+
 
 @bot.command(name='ban')
 async def ban(ctx):
     player = ctx.author.name
     target = parse_args(ctx, '!ban')
 
-    if not is_valid_user(target):
+    if not await is_valid_user(target):
         await ctx.send(f"@{player} casts ban at someone that has long left the area {IMG_NONE}")
         return
 
@@ -184,10 +229,15 @@ async def ban(ctx):
         damage = 40
         await ctx.send(f"{player} {IMG_CRIT} CRITICAL HIT {IMG_CRIT}")
 
+    # saturation weakens your ability to attack and evade, but
+    damage_nat = damage
+    damage = max(damage - players[player].saturation(), 1)  # reduce attack damage
+    damage = min(damage + players[target].saturation(), 39)  # reduce ability to evade
+
     # Get some damage back
     check_weakness(player, do_ban=True)
 
-    dmg_back = 2**players[player].weakness + randint(0, 4)
+    dmg_back = 2 ** players[player].weakness + randint(0, 4)
     players[player].damage(dmg_back)
     await ctx.send(f"@{player} uses {dmg_back} life to cast the ban spell {IMG_SPELL}")
     player_hp = players[player].life()
@@ -197,7 +247,7 @@ async def ban(ctx):
     players[player].last_ban_ts = time()
 
     # Apply damage to target
-    if damage > 0:
+    if (damage_nat > 0) and (damage > 0 or damage_nat == 20):  # nat 20 always hits, nat 1 always misses
         players[target].damage(damage)
         await ctx.send(f"@{player} deals {damage} BAN damage to @{target} {IMG_HIT}")
         target_hp = players[target].life()
@@ -208,7 +258,9 @@ async def ban(ctx):
         else:
             await ctx.send(f"@{player} {player_hp} / @{target} {target_hp} {IMG_HEALTH}")
     else:
-        await ctx.send(f"@{player} raises his hands to cast ban on @{target}. Everyone is holding their breath to see how this battle turns out... but @{target} ducks just in time for the spell to miss them {IMG_MISS}")
+        await ctx.send(
+            f"@{player} raises his hands to cast ban on @{target}. Everyone is holding their breath to see how this battle turns out... but @{target} ducks just in time for the spell to miss them {IMG_MISS}")
+
 
 if __name__ == "__main__":
     bot.run()
